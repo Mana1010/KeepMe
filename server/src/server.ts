4e4e192b01c24bfc5118129942ebba2f2669359c
MANA <108984739+Mana1010@users.noreply.github.com>
2024-03-07 19:52:55 +0800

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(
   cors({
-   origin: "https://keep-me-mern.vercel.app",
+   origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
